0c8268b02c29d859ed1529e08e3adb37e7ba7e38
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2023-03-15 14:45:56 +0100

    Allow git-subtree to run as standalone script
    This allows using tqc's version which supports large repositories
    together with a stock git which has various security fixes and new
    features.
    
    This version is used by bjorn3/rustc_codegen_cranelift
    
    Signed-off-by: Björn Roy Baron <bjorn3_gh@protonmail.com>

--- contrib/subtree/git-subtree.sh
+++ contrib/subtree/git-subtree.sh
@@ -1012,7 +1012,7 @@ cmd_push () {
 		repository=$1
 		refspec=$2
 		echo "git push using: " "$repository" "$refspec"
-		localrev=$(git subtree split --prefix="$prefix") || die
+		localrev=$("$0" split --prefix="$prefix") || die
 		git push "$repository" "$localrev":"refs/heads/$refspec"
 	else
 		die "'$dir' must already exist. Try 'git subtree add'."
